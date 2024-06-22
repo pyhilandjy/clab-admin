b82f5eae92ca991afbb504bd9f522bc822100be7
pyhilandjy <hilandjy@gmail.com>
2024-06-22 23:22:38 +0900

    supabase 인증 추가

---
+++ app/login/page.tsx
@@ -0,0 +1,14 @@
+import { login, signup } from "@/app/login/actions";
+
+export default function LoginPage() {
+  return (
+    <form method="post" action="/api/login">
+      <label htmlFor="email">Email:</label>
+      <input id="email" name="email" type="email" required />
+      <label htmlFor="password">Password:</label>
+      <input id="password" name="password" type="password" required />
+      <button formAction={login}>Log in</button>
+      {/* <button formAction={signup}>Sign up</button> */}
+    </form>
+  );
+}
