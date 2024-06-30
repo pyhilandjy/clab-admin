86133f91229ceebd3f3b29cc743bb237ac390db2
pyhilandjy <hilandjy@gmail.com>
2024-06-30 17:13:24 +0900

    test: 리포트 테스트

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,47 +1,14 @@
-"use client";
-import dayjs from "dayjs";
-import { Button, Select } from "@chakra-ui/react";
-import axios from "axios";
-import { TuiDatePicker } from "nextjs-tui-date-picker";
-import { useEffect, useState } from "react";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { createClient } from "@/utils/supabase/client";
-import { Worker, Viewer } from "@react-pdf-viewer/core";
-import "@react-pdf-viewer/core/lib/styles/index.css";
-
-type User = {
-  user_id: string;
-};
-type SpeechAct = {
-  act_name: string;
-  id: number;
-};
-
-type SentenceLengthData = {
-  "가장 긴 문장": number;
-  "평균 문장 길이": number;
-};
-
-type SentenceLengthResponse = {
-  [speaker: string]: SentenceLengthData;
-};
+'use client';
+import { Button } from '@chakra-ui/react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { createClient } from '@/utils/supabase/client';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
 
 const ReportPage = () => {
-  const backendUrl = process.env.BACKEND_URL;
-  const currentDate = dayjs().format("YYYY-MM-DD");
-  const [users, setUsers] = useState<User[]>([]);
-  const [userId, setUserId] = useState("");
-  const [startDate, setStartDate] = useState("2024-05-01");
-  const [endDate, setEndDate] = useState(currentDate);
-  const [recordTimeData, setRecordTimeData] = useState<any>({});
-  const [sentenceLengthData, setSentenceLengthData] =
-    useState<SentenceLengthResponse>({});
-  const [reportmorpsData, setMorpsReportData] = useState<any>({});
-  const [reportActCountData, setActCountReportData] = useState<any>({});
-  const [speechAct, setSpeechAct] = useState<SpeechAct[]>([]);
-  const [wordcloudimageSrc, setWordcloudImageSrc] = useState<any>(null);
-  const [violinplotimageSrc, setViolinplotImageSrc] = useState(null);
+  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
   const [pdfSrc, setPdfSrc] = useState<string | null>(null);
   const supabase = createClient();
   const router = useRouter();
@@ -49,42 +16,44 @@ const ReportPage = () => {
   useEffect(() => {
     const checkUser = async () => {
       const { data, error } = await supabase.auth.getUser();
-      console.log(data?.user);
       if (error || !data?.user) {
-        router.push("/login");
+        router.push('/login');
       }
     };
     checkUser();
   }, [supabase, router]);
 
-  const handleCreteReportButtonClick = async () => {
-    console.log("Create Report Button Clicked");
+  const handleCreateReportButtonClick = async () => {
+    console.log('Create Report Button Clicked');
     try {
-      const response = await axios.post(
-        `${backendUrl}/report/pdf/`,
+      const response = await axios.get(
+        `${backendUrl}/reports/cf313d6a-201b-4947-9725-acbd2a0d15a6/`,
         {
-          title: "임한나_느낌욕구_2호",
-          user_id: "wnsdyd54",
-        },
-        {
-          responseType: "blob", // Ensure the response is a blob
+          responseType: 'blob', // Ensure the response is a blob
         }
       );
 
-      const blob = new Blob([response.data], { type: "application/pdf" });
-      const url = URL.createObjectURL(blob);
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'report.pdf'); // or any other extension
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+
+      // Set PDF source for viewer
       setPdfSrc(url);
     } catch (error) {
-      console.error("There was an error!", error);
+      console.error('Error creating report:', error);
     }
   };
 
   return (
     <div>
-      <Button onClick={handleCreteReportButtonClick}>Create Report</Button>
+      <Button onClick={handleCreateReportButtonClick}>Create Report</Button>
       {pdfSrc && (
-        <Worker workerUrl="/pdf.worker.min.js">
-          <div style={{ height: "750px" }}>
+        <Worker workerUrl='/pdf.worker.min.js'>
+          <div style={{ height: '750px' }}>
             <Viewer fileUrl={pdfSrc} />
           </div>
         </Worker>
