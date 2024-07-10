132c82075e2c5716c882f64ea6ef635b50d00112
pyhilandjy <hilandjy@gmail.com>
2024-07-10 13:08:05 +0900

    refector: env 환경변수명 변경

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    BACKEND_URL: process.env.BACKEND_URL,
+    BACKEND_URL: process.env.NEXT_PUBLIC_API_URL,
   },
   images: {
-    domains: ["apicall.connects-lab.com", "localhost"],
+    domains: ['apicall.connects-lab.com', 'localhost'],
   },
 };
 
