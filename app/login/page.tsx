6414511b917c806c43ec6aaf700646dea397e480
pyhilandjy <hilandjy@gmail.com>
2024-09-30 13:04:30 +0900

    chore: login css

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,37 @@
 import { login } from '@/app/login/actions';
+import './login.css';
 
 export default function LoginPage() {
   return (
-    <form method='post' action='/api/login'>
-      <label htmlFor='email'>Email:</label>
-      <input id='email' name='email' type='email' required />
-      <label htmlFor='password'>Password:</label>
-      <input id='password' name='password' type='password' required />
-      <button formAction={login}>Log in</button>
-      {/* <button formAction={signup}>Sign up</button> */}
-    </form>
+    <div className='login-container'>
+      <form className='login-form' method='post' action='/api/login'>
+        <h2 className='login-title'>Log In</h2>
+        <label htmlFor='email' className='login-label'>
+          Email:
+        </label>
+        <input
+          id='email'
+          name='email'
+          type='email'
+          className='login-input'
+          required
+        />
+
+        <label htmlFor='password' className='login-label'>
+          Password:
+        </label>
+        <input
+          id='password'
+          name='password'
+          type='password'
+          className='login-input'
+          required
+        />
+
+        <button className='login-button' formAction={login}>
+          Log in
+        </button>
+      </form>
+    </div>
   );
 }
