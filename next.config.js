848ce56ed12005cdd46cbd175ebf59f2481f70b5
pyhilandjy <hilandjy@gmail.com>
2024-10-07 11:37:05 +0900

    chore: supabase update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_API_URL:
-      process.env.NEXT_PUBLIC_API_URL ||
-      'https://admin.apicall.connects-lab.com',
-  },
   images: {
     domains: ['admin.apicall.connects-lab.com', 'localhost'],
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
