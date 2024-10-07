46b40af9cce1e845a5c49822f3b7bc19bb79c193
pyhilandjy <hilandjy@gmail.com>
2024-10-07 14:31:12 +0900

    chore: plan 컴보넌트 스타일 변경

--- app/plan/_components/PlanForm.tsx
+++ app/plan/_components/PlanForm.tsx
@@ -66,122 +66,192 @@ const PlanForm: React.FC<PlanFormProps> = ({
         borderRadius='md'
         p={5}
         mb={10}
-        width='100%'
+        width='70%'
+        position='absolute'
+        top='3%'
+        left='16%'
       >
-        <Heading as='h2' size='md' mb={4}>
-          {isEdit ? '플랜 수정' : '플랜 추가'}
-        </Heading>
+        <Box
+          display='flex'
+          justifyContent='space-between'
+          alignItems='center'
+          mb={4}
+        >
+          <Heading as='h2' size='md'>
+            {isEdit ? '플랜 수정' : '플랜 추가'}
+          </Heading>
+          <Button type='submit' colorScheme='blue' width='150px'>
+            {isEdit ? '수정' : '생성'}
+          </Button>
+        </Box>
+
         <SimpleGrid columns={2} spacing={4} mb={4}>
           <GridItem colSpan={2}>
             <FormControl id='plan_name' isRequired>
-              <FormLabel>플랜 이름</FormLabel>
-              <Input
-                type='text'
-                value={planName}
-                name='planName'
-                onChange={onChange}
-              />
+              <Box display='flex' alignItems='center'>
+                <FormLabel width='128px' mb={0}>
+                  플랜 이름
+                </FormLabel>
+                <Input
+                  type='text'
+                  value={planName}
+                  name='planName'
+                  onChange={onChange}
+                />
+              </Box>
             </FormControl>
           </GridItem>
         </SimpleGrid>
+
         <SimpleGrid columns={2} spacing={4} mb={4}>
           <GridItem colSpan={1}>
             <FormControl id='price'>
-              <FormLabel>가격</FormLabel>
-              <Input
-                type='number'
-                value={price}
-                name='price'
-                onChange={onChange}
-              />
+              <Box display='flex' alignItems='center'>
+                <FormLabel width='157px' mb={0}>
+                  가격
+                </FormLabel>
+                <Input
+                  type='number'
+                  value={price}
+                  name='price'
+                  onChange={onChange}
+                />
+              </Box>
             </FormControl>
           </GridItem>
           <GridItem colSpan={1}>
             <FormControl id='day' isRequired>
-              <FormLabel>일수</FormLabel>
-              <Input type='number' value={day} name='day' onChange={onChange} />
+              <Box display='flex' alignItems='center'>
+                <FormLabel width='150px' mb={0}>
+                  미션 갯수
+                </FormLabel>
+                <Input
+                  type='number'
+                  value={day}
+                  name='day'
+                  onChange={onChange}
+                />
+              </Box>
             </FormControl>
           </GridItem>
         </SimpleGrid>
-        <SimpleGrid columns={2} spacing={4} mb={4}>
-          <GridItem colSpan={1}>
-            <FormControl id='start_age_month'>
-              <FormLabel>시작 나이 (개월)</FormLabel>
-              <Input
-                type='number'
-                value={startAgeMonth}
-                name='startAgeMonth'
-                onChange={onChange}
-              />
-            </FormControl>
-          </GridItem>
-          <GridItem colSpan={1}>
-            <FormControl id='end_age_month'>
-              <FormLabel>종료 나이 (개월)</FormLabel>
-              <Input
-                type='number'
-                value={endAgeMonth}
-                name='endAgeMonth'
-                onChange={onChange}
-              />
-            </FormControl>
-          </GridItem>
-        </SimpleGrid>
+
         <SimpleGrid columns={2} spacing={4} mb={4}>
           <GridItem colSpan={1}>
             <FormControl id='type'>
-              <FormLabel>타입</FormLabel>
-              <Select
-                placeholder='타입을 선택하세요'
-                value={type}
-                name='type'
-                onChange={onChange}
-              >
-                <option value='달성형'>달성형</option>
-                <option value='기간형'>기간형</option>
-              </Select>
+              <Box display='flex' alignItems='center'>
+                <FormLabel width='150px' mb={0}>
+                  타입
+                </FormLabel>
+                <Select
+                  placeholder='타입을 선택하세요'
+                  value={type}
+                  name='type'
+                  onChange={onChange}
+                >
+                  <option value='달성형'>달성형</option>
+                  <option value='기간형'>기간형</option>
+                </Select>
+              </Box>
             </FormControl>
           </GridItem>
           <GridItem colSpan={1}>
             <FormControl id='tags'>
-              <FormLabel>태그 (콤마로 구분)</FormLabel>
-              <Input type='text' value={tags} name='tags' onChange={onChange} />
+              <Box display='flex' alignItems='center'>
+                <FormLabel width='150px' mb={0}>
+                  메타데이터
+                </FormLabel>
+                <Input
+                  type='text'
+                  value={tags}
+                  name='tags'
+                  onChange={onChange}
+                  placeholder='컴마로 구분해 주세요'
+                />
+              </Box>
             </FormControl>
           </GridItem>
         </SimpleGrid>
+
         <SimpleGrid columns={2} spacing={4} mb={4}>
           <GridItem colSpan={1}>
             <FormControl id='main_category'>
-              <FormLabel>메인 카테고리</FormLabel>
-              <Select
-                placeholder='메인 카테고리를 선택하세요'
-                value={selectedMainCategory}
-                onChange={(e) => handleMainCategoryChange(e.target.value)}
-              >
-                {mainCategories.map((category) => (
-                  <option key={category.id} value={category.id}>
-                    {category.name}
-                  </option>
-                ))}
-              </Select>
+              <Box display='flex' alignItems='center'>
+                <FormLabel width='150px' mb={0}>
+                  메인 카테고리
+                </FormLabel>
+                <Select
+                  placeholder='메인 카테고리를 선택하세요'
+                  value={selectedMainCategory}
+                  onChange={(e) => handleMainCategoryChange(e.target.value)}
+                >
+                  {mainCategories.map((category) => (
+                    <option key={category.id} value={category.id}>
+                      {category.name}
+                    </option>
+                  ))}
+                </Select>
+              </Box>
             </FormControl>
           </GridItem>
           <GridItem colSpan={1}>
             <FormControl id='sub_category' isRequired>
-              <FormLabel>서브 카테고리</FormLabel>
-              <Select
-                placeholder='서브 카테고리를 선택하세요'
-                value={selectedSubCategory}
-                name='selectedSubCategory'
-                onChange={onSubCategoryChange} // 수정된 부분
-                isDisabled={!selectedMainCategory}
-              >
-                {subCategories.map((category) => (
-                  <option key={category.id} value={category.id}>
-                    {category.name}
-                  </option>
-                ))}
-              </Select>
+              <Box display='flex' alignItems='center'>
+                <FormLabel width='150px' mb={0}>
+                  서브 카테고리
+                </FormLabel>
+                <Select
+                  placeholder='서브 카테고리를 선택하세요'
+                  value={selectedSubCategory}
+                  name='selectedSubCategory'
+                  onChange={onSubCategoryChange}
+                  isDisabled={!selectedMainCategory}
+                >
+                  {subCategories.map((category) => (
+                    <option key={category.id} value={category.id}>
+                      {category.name}
+                    </option>
+                  ))}
+                </Select>
+              </Box>
+            </FormControl>
+          </GridItem>
+        </SimpleGrid>
+
+        <SimpleGrid columns={2} spacing={4} mb={4}>
+          <GridItem colSpan={1}>
+            <FormControl id='recommended_age'>
+              <Box display='flex' alignItems='center'>
+                <FormLabel width='140px' mb={0}>
+                  추천 월령
+                </FormLabel>
+                <Box display='flex' gap={4}>
+                  <Input
+                    type='number'
+                    value={startAgeMonth}
+                    name='startAgeMonth'
+                    onChange={onChange}
+                    placeholder='시작 나이'
+                  />
+                  <Input
+                    type='number'
+                    value={endAgeMonth}
+                    name='endAgeMonth'
+                    onChange={onChange}
+                    placeholder='종료 나이'
+                  />
+                </Box>
+              </Box>
+            </FormControl>
+          </GridItem>
+          <GridItem colSpan={1}>
+            <FormControl id='day' isRequired>
+              <Box display='flex' alignItems='center'>
+                <FormLabel width='150px' mb={0}>
+                  레이블
+                </FormLabel>
+                <Select placeholder='준비중' isDisabled></Select>
+              </Box>
             </FormControl>
           </GridItem>
         </SimpleGrid>
@@ -200,9 +270,6 @@ const PlanForm: React.FC<PlanFormProps> = ({
           </FormControl>
         </Box>
       </Box>
-      <Button type='submit' colorScheme='blue' mt={4}>
-        {isEdit ? '수정' : '생성'}
-      </Button>
     </form>
   );
 };
