5a5d49d97f163a98ef5d588ff8c87ce1a67a0abd
pyhilandjy <hilandjy@gmail.com>
2024-10-07 11:37:52 +0900

    refactor: Separate login and signup actions in login/actions.ts

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,20 @@
-import { NextResponse, type NextRequest } from 'next/server';
+import { type NextRequest } from 'next/server';
 
 import { updateSession } from '@/utils/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
-  const response = await updateSession(request);
-
-  if (response) {
-    return response;
-  }
-  const { pathname } = request.nextUrl;
-
-  if (!response && pathname !== '/login') {
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
-
-  return NextResponse.next();
+  return await updateSession(request);
 }
 
 export const config = {
   matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
