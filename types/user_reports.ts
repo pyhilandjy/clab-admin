1683924b3d21b33d6286dda2aed362a82ecfe943
pyhilandjy <hilandjy@gmail.com>
2024-12-11 00:57:10 +0900

    chore: wordcloud type add

--- types/user_reports.ts
+++ types/user_reports.ts
@@ -1,3 +1,10 @@
-export interface WordcloudData {
-  data: string;
+export interface WordCounts {
+  [word: string]: number;
 }
+
+export interface SpeakerData {
+  speaker: string;
+  word_counts: WordCounts;
+}
+
+export type WordcloudData = SpeakerData[];
