bf685ce6a0951e94aef1d786402f4aba5f60e923
pyhilandjy <hilandjy@gmail.com>
2024-12-11 00:56:39 +0900

    chore: 워드클라우드 데이터 fatch api

--- api/user_report.ts
+++ api/user_report.ts
@@ -1,5 +1,19 @@
 import api from '@/lib/api';
 import { WordcloudData } from '@/types/user_reports';
 
-export const fetchWordCloudData = () =>
-  api.get<WordcloudData[]>('/wordcloud/data');
+export const fetchWordCloudData = async (
+  userReportsId: string,
+): Promise<WordcloudData> => {
+  try {
+    const response = await api.get<WordcloudData>('/wordcloud/data', {
+      params: { user_reports_id: userReportsId },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(
+      `Error fetching word cloud data for ID ${userReportsId}:`,
+      error,
+    );
+    throw error;
+  }
+};
