e2dd50ad38caa18715bfd590922e04f6a96c7be8
pyhilandjy <hilandjy@gmail.com>
2024-12-10 17:47:09 +0900

    refactor: 리포트관리 페이지 쿼리파라미터 강제 입력

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from 'next/navigation';
 import { ReactNode } from 'react';
 
 import { Link } from '@chakra-ui/next-js';
@@ -9,28 +10,30 @@ interface LayoutProps {
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const searchParams = useSearchParams();
+  const currentParams = searchParams.toString() || 'page=1';
   return (
     <div className={styles.container}>
       <nav className={styles.sidebar}>
         <div className={styles.adminHeader}>Admin</div>
         <ul>
           <li>
-            <Link href='/'>대시보드</Link>
+            <Link href={`/`}>대시보드</Link>
           </li>
           <li>
-            <Link href='/report'>리포트</Link>
+            <Link href={`/report`}>리포트</Link>
           </li>
           <li>
-            <Link href='/stt-edit'>STT수정</Link>
+            <Link href={`/stt-edit`}>STT수정</Link>
           </li>
           <li>
-            <Link href='/plan'>패키지관리</Link>
+            <Link href={`/plan`}>패키지관리</Link>
           </li>
           <li>
-            <Link href='/report-manage'>리포트관리</Link>
+            <Link href={`/report-manage?${currentParams}`}>리포트관리</Link>
           </li>
           <li>
-            <Link href='/user-manage'>회원관리</Link>
+            <Link href={`/user-manage?${currentParams}`}>회원관리</Link>
           </li>
         </ul>
       </nav>
