b0861f67b9d3d15efeefc4fab9f10e9318a37cda
pyhilandjy <hilandjy@gmail.com>
2025-01-14 22:20:11 +0900

    feat: 추가 설명, 요약, 스케줄, 이미지 추가기능 추가, createplan data form 변경 api 통합

--- api/plan.ts
+++ api/plan.ts
@@ -13,6 +13,39 @@ export const updatePlanStatus = (planId: string, status: string) =>
 
 export const deletePlan = (planId: string) => api.delete(`/plans/${planId}`);
 
+export const uploadDescriptionImage = (
+  planId: string,
+  description_image_name: string,
+  image: File,
+) => {
+  const formData = new FormData();
+  formData.append('description_image_name', description_image_name);
+  formData.append('image', image);
+  return api.patch(`/plans/${planId}/description/image`, formData);
+};
+
+export const uploadThumbnailImage = (
+  planId: string,
+  thumbnail_image_name: string,
+  image: File,
+) => {
+  const formData = new FormData();
+  formData.append('thumbnail_image_name', thumbnail_image_name);
+  formData.append('image', image);
+  return api.patch(`/plans/${planId}/thumbnail/image`, formData);
+};
+
+export const uploadScheduleImage = (
+  planId: string,
+  schedule_image_name: string,
+  image: File,
+) => {
+  const formData = new FormData();
+  formData.append('schedule_image_name', schedule_image_name);
+  formData.append('image', image);
+  return api.patch(`/plans/${planId}/schedule/image`, formData);
+};
+
 //plan/add.tsx
 export const fetchMainCategories = () =>
   api.get<Category[]>('/categories/main/');
@@ -20,7 +53,8 @@ export const fetchMainCategories = () =>
 export const fetchSubCategories = (mainCategoryId: string) =>
   api.get(`/categories/sub/${mainCategoryId}`);
 
-export const createPlan = (payload: any) => api.post<Plan>('/plans/', payload);
+export const createPlan = (formData: FormData) =>
+  api.post<Plan>('/plans/', formData);
 
 export const fetchPlan = (planId: string) => api.get<Plan>(`/plans/${planId}`);
 
