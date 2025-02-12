79738f42c091c62a87d5ad99609dc1d370a5f1a4
pyhilandjy <hilandjy@gmail.com>
2025-02-12 16:18:17 +0900

    chore: userplan type

---
+++ types/user_plans.ts
@@ -0,0 +1,9 @@
+export type UserPlan = {
+  user_plans_id: string;
+  user_id: string;
+  plan_created_at: string;
+  status: string;
+  plan_name: string;
+  user_name: string;
+  user_created_at: string;
+};
